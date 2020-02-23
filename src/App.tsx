f9ad779cc0d6524712a245f0a0331204ef2b3824
voxmatt <matt@getclockwise.com>
2020-02-22 16:30:19 -0800

    sending stuff to Slack works

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { WebClient } from '@slack/web-api';
 import logo from './logo.svg';
 import './App.css';
 
+function sendMessage(slackClient: WebClient, message: string) {
+  (async () => {
+
+    try {
+      // Use the `chat.postMessage` method to send a message from this app
+      await slackClient.chat.postMessage({
+        channel: '#test-channel',
+        text: message,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    console.log('Message posted!');
+  })();
+}
+
+function SlackClient({ slackToken }: { slackToken: string }) {
+  const web = new WebClient(slackToken);
+  return (
+    <button onClick={() => sendMessage(web, "hello world")}>Send Message</button>
+  )
+}
+
 function App() {
+  const [slackToken, setSlackToken] = useState('');
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label>Slack Token</label>
+        <input value={slackToken} onChange={(e) => setSlackToken(e.target.value)} />
+        <SlackClient slackToken={slackToken} />
       </header>
     </div>
   );
