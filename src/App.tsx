268df25b80fe29754b4eb7c4c7be78e6d276f80f
voxmatt <matt@getclockwise.com>
2020-02-23 10:44:25 -0800

    giving up on logging in with Slack

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,18 @@ import { WebClient } from '@slack/web-api';
 import logo from './logo.svg';
 import './App.css';
 
+const CLIENT_ID = '88887328098.404244181942';
+
 function sendMessage(slackClient: WebClient, message: string) {
   (async () => {
 
     try {
-      // Use the `chat.postMessage` method to send a message from this app
-      await slackClient.chat.postMessage({
-        channel: '#test-channel',
-        text: message,
-      });
+      const result = await slackClient.channels.list();
+      console.log(result);
     } catch (error) {
       console.log(error);
     }
 
-    console.log('Message posted!');
   })();
 }
 
@@ -27,14 +25,40 @@ function SlackClient({ slackToken }: { slackToken: string }) {
   )
 }
 
+function saveSlackToken(slackToken: string) {
+  window.localStorage.setItem('slackToken', slackToken);
+}
+
+function getSlackToken() {
+  return window.localStorage.getItem('slackToken');
+}
+
+function reportWindowLocation() {
+  console.log(window.location);
+  const params = (new URL(document.location.href)).searchParams;
+  const code = params.get('code');
+  if (code) {
+    maybeGetAccessToken(code);
+  }
+}
+
+async function maybeGetAccessToken(code: string) {
+  const requestURI = `https://slack.com/api/oauth.access?client_id=${CLIENT_ID}&code=${code}`;
+  const result = await fetch(requestURI);
+  console.log(result);
+}
+
 function App() {
-  const [slackToken, setSlackToken] = useState('');
+  const localStorageSlackToken = getSlackToken();
+  const [slackToken, setSlackToken] = useState(localStorageSlackToken || '');
+  reportWindowLocation();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <label>Slack Token</label>
         <input value={slackToken} onChange={(e) => setSlackToken(e.target.value)} />
+        <button onClick={() => saveSlackToken(slackToken)}>Save Token</button>
         <SlackClient slackToken={slackToken} />
       </header>
     </div>
